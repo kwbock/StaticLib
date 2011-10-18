6e004300818b3b141814107862486514eb0c822f
Kyle Bock <kylewbock@gmail.com>
2011-10-18 10:01:26 -0400

    Initial Commit

---
+++ StaticLib/StaticLib.m
@@ -0,0 +1,23 @@
+//
+//  StaticLib.m
+//  StaticLib
+//
+//  Created by Kyle Bock on 10/18/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "StaticLib.h"
+
+@implementation StaticLib
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
